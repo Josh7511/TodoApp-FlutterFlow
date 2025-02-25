1422e18b0a4a6ed7af7887914aea24fce280ec96
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-25 01:59:23 +0000

    AddedAPICall

--- lib/components/add_task_widget.dart
+++ lib/components/add_task_widget.dart
@@ -48,15 +48,15 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: const AlignmentDirectional(0.0, 0.0),
+      alignment: AlignmentDirectional(0.0, 0.0),
       child: Container(
         width: 400.0,
-        constraints: const BoxConstraints(
+        constraints: BoxConstraints(
           maxWidth: 400.0,
         ),
         decoration: BoxDecoration(
           color: FlutterFlowTheme.of(context).secondaryBackground,
-          borderRadius: const BorderRadius.only(
+          borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(0.0),
             bottomRight: Radius.circular(0.0),
             topLeft: Radius.circular(24.0),
@@ -68,7 +68,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: EdgeInsets.all(24.0),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -98,7 +98,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                       letterSpacing: 0.0,
                     ),
               ),
-              SizedBox(
+              Container(
                 width: double.infinity,
                 child: TextFormField(
                   controller: _model.titleTextController,
@@ -125,7 +125,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                       borderRadius: BorderRadius.circular(24.0),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
+                      borderSide: BorderSide(
                         color: Color(0x00000000),
                         width: 1.0,
                       ),
@@ -148,7 +148,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                     filled: true,
                     fillColor: FlutterFlowTheme.of(context).secondaryBackground,
                     contentPadding:
-                        const EdgeInsetsDirectional.fromSTEB(24.0, 26.0, 24.0, 26.0),
+                        EdgeInsetsDirectional.fromSTEB(24.0, 26.0, 24.0, 26.0),
                   ),
                   style: FlutterFlowTheme.of(context).bodyMedium.override(
                         fontFamily: 'Inter',
@@ -159,7 +159,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                       _model.titleTextControllerValidator.asValidator(context),
                 ),
               ),
-              SizedBox(
+              Container(
                 width: double.infinity,
                 child: TextFormField(
                   controller: _model.detailsTextController,
@@ -186,7 +186,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                       borderRadius: BorderRadius.circular(24.0),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
+                      borderSide: BorderSide(
                         color: Color(0x00000000),
                         width: 1.0,
                       ),
@@ -209,7 +209,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                     filled: true,
                     fillColor: FlutterFlowTheme.of(context).secondaryBackground,
                     contentPadding:
-                        const EdgeInsetsDirectional.fromSTEB(24.0, 26.0, 24.0, 26.0),
+                        EdgeInsetsDirectional.fromSTEB(24.0, 26.0, 24.0, 26.0),
                   ),
                   style: FlutterFlowTheme.of(context).bodyMedium.override(
                         fontFamily: 'Inter',
@@ -240,16 +240,16 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                   Navigator.pop(context);
                 },
                 text: 'Add Task',
-                icon: const Icon(
+                icon: Icon(
                   Icons.add,
                   size: 30.0,
                 ),
                 options: FFButtonOptions(
                   width: double.infinity,
                   height: 70.0,
-                  padding: const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+                  padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
                   iconPadding:
-                      const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
                   color: FlutterFlowTheme.of(context).primary,
                   textStyle: FlutterFlowTheme.of(context).labelMedium.override(
                         fontFamily: 'Inter',
@@ -266,7 +266,7 @@ class _AddTaskWidgetState extends State<AddTaskWidget> {
                   borderRadius: BorderRadius.circular(24.0),
                 ),
               ),
-            ].divide(const SizedBox(height: 24.0)),
+            ].divide(SizedBox(height: 24.0)),
           ),
         ),
       ),
